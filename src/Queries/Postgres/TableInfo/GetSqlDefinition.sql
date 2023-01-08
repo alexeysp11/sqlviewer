64e78c17f294b4aea9d39cc1c1bccb4a6ef5789c
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-01-08 20:07:40 +0500

    Corrected table definition for PostgreSQL

--- src/Queries/Postgres/TableInfo/GetSqlDefinition.sql
+++ src/Queries/Postgres/TableInfo/GetSqlDefinition.sql
@@ -10,7 +10,7 @@ DECLARE
 BEGIN
     i := 0; 
     SELECT COUNT(*) INTO lNumRec FROM information_schema.columns WHERE table_schema LIKE aSchemaName AND table_name LIKE aTableName; 
-    lResult := 'CREATE TABLE ' || aSchemaName || '.' || aTableName || ' (' || chr(10); 
+    lResult := 'CREATE TABLE ' || aSchemaName || '.' || aTableName || chr(10) || '(' || chr(10); 
     FOR rec IN (
         SELECT 
             column_name, 
@@ -23,15 +23,15 @@ BEGIN
     )
     LOOP
         i := i + 1; 
-        lResult := lResult || rec.column_name || ' ' || rec.data_type; 
+        lResult := lResult || '    ' || rec.column_name || ' ' || rec.data_type; 
         IF UPPER(rec.data_type) LIKE '%CHAR%VAR%' THEN 
             lResult := lResult || '(' || rec.character_maximum_length || ')'; 
         END IF; 
         IF rec.column_default IS NOT NULL AND rec.column_default <> '' THEN 
-            lResult := lResult || 'DEFAULT ' || rec.column_default; 
+            lResult := lResult || ' DEFAULT ' || rec.column_default; 
         END IF; 
         IF i = lNumRec THEN 
-            lResult := lResult || ');'; 
+            lResult := lResult || chr(10) || ');'; 
         ELSE 
             lResult := lResult || ', ' || chr(10); 
         END IF; 
