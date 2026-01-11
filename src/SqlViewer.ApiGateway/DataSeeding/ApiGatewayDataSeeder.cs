89050457ab53f07a48a854343ee900accf2e85f0
alexeysp11 <alexeyspiridon1111@gmail.com>
2026-01-11 13:01:27 +0500

    issue-21: api gateway, data seeder

--- src/SqlViewer.ApiGateway/DataSeeding/ApiGatewayDataSeeder.cs
+++ src/SqlViewer.ApiGateway/DataSeeding/ApiGatewayDataSeeder.cs
@@ -4,6 +4,7 @@ using SqlViewer.ApiGateway.DbContexts;
 using SqlViewer.ApiGateway.Services;
 using SqlViewer.Common.Models;
 using VelocipedeUtils.Shared.DbOperations.Enums;
+using static SqlViewer.Common.Constants.ConfigurationKeys;
 
 namespace SqlViewer.ApiGateway.DataSeeding;
 
@@ -15,39 +16,72 @@ public class ApiGatewayDataSeeder(
 {
     public async Task InitializeAsync()
     {
-        string? encryptionKey = config["EncryptionSettings:Key"];
+        string? encryptionKey = config[Encryption.Key];
         if (string.IsNullOrEmpty(encryptionKey))
         {
-            throw new Exception("Encryption key is missing from configuration!");
+            throw new Exception("Encryption key is missing from configuration");
         }
 
         await context.Database.MigrateAsync();
 
-        if (!await context.Users.AnyAsync())
+        User admin = await CreateAdminUserAsync();
+        await CreateMetadataDataSource(admin);
+
+        await context.SaveChangesAsync();
+    }
+
+    private async Task<User> CreateAdminUserAsync()
+    {
+        string? adminUsername = config[DefaultUserCredentials.AdminUsername];
+        if (string.IsNullOrEmpty(adminUsername))
         {
-            // 1. Create admin.
-            User admin = new()
+            throw new Exception("Admin username is missing from configuration");
+        }
+        User? admin = await context.Users.FirstOrDefaultAsync(x => x.Username == adminUsername);
+        if (admin is null)
+        {
+            string? adminPassword = config[DefaultUserCredentials.AdminPassword];
+            if (string.IsNullOrEmpty(adminPassword))
             {
-                Username = "admin",
-                PasswordHash = passwordHasher.HashPassword(null!, "admin")
+                throw new Exception("Admin password is missing from configuration");
+            }
+            admin = new()
+            {
+                Username = adminUsername,
+                PasswordHash = passwordHasher.HashPassword(null!, adminPassword)
             };
             context.Users.Add(admin);
+        }
+        return admin;
+    }
 
-            // 2. Create data source.
-            string metadataConnectionString = config.GetConnectionString("MetadataConnection")
+    private async Task CreateMetadataDataSource(User admin)
+    {
+        string? dataSourceName = config[DefaultDataSources.MetadataDbName];
+        if (string.IsNullOrEmpty(dataSourceName))
+        {
+            throw new Exception("Metadata datasource name is missing from configuration");
+        }
+        DataSource? dataSource = await context.DataSources.FirstOrDefaultAsync(x => x.Name == dataSourceName);
+        if (dataSource is null)
+        {
+            string metadataConnectionString = config.GetConnectionString(ConnectionStrings.Metadata)
                 ?? throw new Exception("Metadata connection string could not be null");
-            DataSource dataSource = new()
+            string? dataSourceDescription = config[DefaultDataSources.MetadataDbDescription];
+            if (string.IsNullOrEmpty(dataSourceDescription))
             {
-                Name = "pg-metadata-db",
-                Description = "PostgreSQL database for metadata",
+                throw new Exception("Metadata datasource description is missing from configuration");
+            }
+            dataSource = new()
+            {
+                Name = dataSourceName,
+                Description = dataSourceDescription,
                 EncryptedConnectionString = encryptionService.Encrypt(metadataConnectionString),
                 Owner = admin,
                 DbType = VelocipedeDatabaseType.PostgreSQL,
             };
             dataSource.Permissions = [new() { User = admin, DataSource = dataSource, AccessLevel = AccessLevelType.Admin }];
             context.DataSources.Add(dataSource);
-
-            await context.SaveChangesAsync();
         }
     }
 }
---
+++ src/SqlViewer.Common/Constants/ConfigurationKeys.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SqlViewer.Common.Constants;
+
+[SuppressMessage("Design", "CA1034:Nested types should not be visible", Justification = "<Pending>")]
+public static class ConfigurationKeys
+{
+    public static class Encryption
+    {
+        public const string Section = "EncryptionSettings";
+        public const string Key = $"{Section}:Key";
+    }
+
+    public static class DefaultUserCredentials
+    {
+        public const string Section = "DefaultUserCredentials";
+        public const string AdminUsername = $"{Section}:AdminUsername";
+        public const string AdminPassword = $"{Section}:AdminPassword";
+    }
+
+    public static class DefaultDataSources
+    {
+        public const string Section = "DefaultDataSources";
+        public const string MetadataDbName = $"{Section}:MetadataDbName";
+        public const string MetadataDbDescription = $"{Section}:MetadataDbDescription";
+    }
+
+    public static class ConnectionStrings
+    {
+        public const string Metadata = "MetadataConnection";
+    }
+}
