ae03aa5de186924666ff88917d8f4686492eb1f6
alexeysp11 <alexeyspiridon1111@gmail.com>
2026-02-21 21:10:43 +0500

    issue-14: security, method for mapping

--- src/SqlViewer.Security/Mappings/SeedMapper.cs
+++ src/SqlViewer.Security/Mappings/SeedMapper.cs
@@ -15,7 +15,7 @@ public partial class SeedMapper
     [MapperIgnoreTarget(nameof(User.RefreshToken))]
     [MapperIgnoreTarget(nameof(User.RefreshTokenExpiry))]
     [MapValue(nameof(User.PasswordHash), DefaultPassword)]
-    [MapProperty(nameof(UserSeedDto.Role), nameof(User.Role))]
+    [MapProperty(nameof(UserSeedDto.Role), nameof(User.Role), Use = nameof(MapRole))]
     public partial User MapToUser(UserSeedDto dto);
 
 #pragma warning disable IDE0051 // Remove unused private members
